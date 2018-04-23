c068c803a375aab734dc44171fbdfe1fe8681270
monopro <bbtmba@gmail.com>
2018-04-23 16:25:18 +0900

    update

---
+++ Vuforia_Labo01/Assets/Scripts/BallHit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHit : MonoBehaviour {
+
+	public AudioClip hitsound;
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Player") {
+			AudioSource.PlayClipAtPoint (hitsound, transform.position);
+		}
+	}
+}
