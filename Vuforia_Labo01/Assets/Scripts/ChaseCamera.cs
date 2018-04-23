c58e1e175d7197cb8b4c71ad689bf85f82a2b8be
monopro <bbtmba@gmail.com>
2018-04-23 16:14:47 +0900

    name change
    プロジェクトの名称変更

---
+++ Vuforia_Labo01/Assets/Scripts/ChaseCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseCamera : MonoBehaviour {
+
+	public GameObject target;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - target.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = target.transform.position + offset;
+		
+	}
+}
