934181aec6bac1cee615c96d66bc7e55363843a1
monopro <bbtmba@gmail.com>
2018-04-23 17:14:55 +0900

    update

--- Vuforia_Labo01/Assets/Scripts/Rotate.cs
+++ Vuforia_Labo01/Assets/Scripts/Rotate.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class Rotate : MonoBehaviour {
 
 	public Vector3 rot;
+	private float rotSwitch = 1;
 
 	void Update () {
-		transform.Rotate (new Vector3 (rot.x, rot.y, rot.z) * Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.Z)) {
+			rotSwitch = -1;
+		} else {
+			rotSwitch = 1;
+		}
+			
+		transform.Rotate (new Vector3 (rot.x, rot.y * rotSwitch, rot.z) * Time.deltaTime);
 	}
 }
