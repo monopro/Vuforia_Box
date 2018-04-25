11c40e3e31810368d5550723755d988ff83e3e0d
monopro <bbtmba@gmail.com>
2018-04-25 14:15:44 +0900

    update

---
+++ Vuforia_Labo01/Assets/Scripts/ManualRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualRotate : MonoBehaviour {
+
+	void Start(){
+
+		// カーソルを非表示にしてロックをかける。
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update () {
+		transform.Rotate (0, Input.GetAxis ("Mouse X") * 2, 0);
+	}
+}
